153c73cb5786917aaec2312711358857b1ccfb4c
Namiq Əbilov <111847013+namigabilov@users.noreply.github.com>
2023-04-19 21:44:31 +0400

    Added New File

---
+++ ConnectionString(19.04.2023)/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.qr605DataSet = new WindowsFormsApp4.qr605DataSet();
+            this.t2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.t2TableAdapter = new WindowsFormsApp4.qr605DataSetTableAdapters.t2TableAdapter();
+            this.acarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qr605DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save inf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(638, 205);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 242);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(312, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.acarDataGridViewTextBoxColumn,
+            this.telDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.t2BindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(682, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(406, 170);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // qr605DataSet
+            // 
+            this.qr605DataSet.DataSetName = "qr605DataSet";
+            this.qr605DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // t2BindingSource
+            // 
+            this.t2BindingSource.DataMember = "t2";
+            this.t2BindingSource.DataSource = this.qr605DataSet;
+            // 
+            // t2TableAdapter
+            // 
+            this.t2TableAdapter.ClearBeforeFill = true;
+            // 
+            // acarDataGridViewTextBoxColumn
+            // 
+            this.acarDataGridViewTextBoxColumn.DataPropertyName = "acar";
+            this.acarDataGridViewTextBoxColumn.HeaderText = "acar";
+            this.acarDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.acarDataGridViewTextBoxColumn.Name = "acarDataGridViewTextBoxColumn";
+            this.acarDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // telDataGridViewTextBoxColumn
+            // 
+            this.telDataGridViewTextBoxColumn.DataPropertyName = "tel";
+            this.telDataGridViewTextBoxColumn.HeaderText = "tel";
+            this.telDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.telDataGridViewTextBoxColumn.Name = "telDataGridViewTextBoxColumn";
+            this.telDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(761, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 48);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qr605DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private qr605DataSet qr605DataSet;
+        private System.Windows.Forms.BindingSource t2BindingSource;
+        private qr605DataSetTableAdapters.t2TableAdapter t2TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn acarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ConnectionString(19.04.2023)/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Goster(string S)
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(S,con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        public SqlConnection con = new SqlConnection("Data source=NAMIQ\\SQLEXPRESS01;Initial catalog=qr605;Integrated security=SSPI");
+        SqlCommand com;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qr605DataSet.t2' table. You can move, or remove it, as needed.
+            this.t2TableAdapter.Fill(this.qr605DataSet.t2);
+            Goster("Select* from t1");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Insert into t1(telebe,qr,bal)values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')");
+            con.Open();
+            com = new SqlCommand("Insert into t1(telebe,qr,bal)values('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox3.Text+"')",con);
+            com.ExecuteNonQuery();
+            con.Close();
+            Goster("Select* from t1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Search")
+            {
+                Goster("Select* from t1 where telebe like '" + textBox1.Text + "'");
+                button2.Text = "All View";
+            }
+            else {
+                Goster("Select* from t1 ");
+                button2.Text = "Search";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            com = new SqlCommand("update t1 set bal='"+textBox3.Text+"' where telebe like '"+textBox1.Text+"'", con);
+            com.ExecuteNonQuery();
+            con.Close();
+            Goster("Select* from t1");
+        }
+    }
+}
